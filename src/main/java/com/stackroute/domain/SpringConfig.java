1fcc41acdc3c0f9d75b2559e41d25dcd2bb9fbe1
harikapa <51950463+harikapa@users.noreply.github.com>
2019-07-19 10:53:16 +0530

    Update SpringConfig.java

--- src/main/java/com/stackroute/domain/SpringConfig.java
+++ src/main/java/com/stackroute/domain/SpringConfig.java
@@ -4,9 +4,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+//registering it as configuration class
 @Configuration
-@ComponentScan(basePackages = "com.stackroute.domain")
+
+//path of property file
 @PropertySource(value = "classpath:ClassPropertieValues.properties")
+@ComponentScan(basePackages = "com.stackroute.domain")
 public class SpringConfig {
 
 
